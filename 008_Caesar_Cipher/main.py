e36336826f01cc43b2f4baee0ca5c64934d04530
J MacPherson <jtdmacpherson@pinkieboi.com>
2024-08-13 19:46:02 +0100

    Day 8

--- 008_Caesar_Cipher/main.py
+++ 008_Caesar_Cipher/main.py
@@ -2,30 +2,24 @@ import string
 
 alphabet = string.ascii_lowercase * 2
 message = input("Message to encrypt / decrypt:\n")
+key = int(input("Encryption key:\n"))
 mode = int(input("Enter 1 to encrypt or 2 to decrypt\n"))
-key = int(input("Enter your key\n"))
 
 
-def cipher(message, mode, key):
-    output = []
-    if mode == 1:
-        for letter in message:
-            if letter == " ":
-                output.append(" ")
-            else:
-                out_index = alphabet.index(letter) + key
-                output.append(alphabet[out_index])
-    elif mode == 2:
-       for letter in message:
-            if letter == " ":
-                output.append(" ")
-            else:
-                out_index = alphabet.index(letter) - key
-                output.append(alphabet[out_index])
-    else:
-        print("Invalid Mode")
-    
-    print(f"Message:  {"".join(output)}")
+def cipher(message, key, mode):
+    output = ""
+    for letter in message:
+        if letter == " ":
+            output += letter
+        elif mode == 1:
+            i = alphabet.index(letter) + key
+            output += alphabet[i]
+        elif mode == 2:
+            i = alphabet.index(letter) - key
+            output += alphabet[i]
+        else:
+            print("Invalid mode")
+    print(output)
 
 
-cipher(message, mode, key)
+cipher(message, key, mode)
