bd160c4041463a3bcfa505c29f27c14247d92983
J MacPherson <jtdmacpherson@pinkieboi.com>
2024-09-05 16:32:40 +0100

    add comments from explainer

--- 023_Turtle_Crossing_Capstone_Project/cars.py
+++ 023_Turtle_Crossing_Capstone_Project/cars.py
@@ -6,10 +6,9 @@ STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 2
 
 
-class CarManager(Turtle):
+class CarManager:
 
     def __init__(self):
-        super().__init__()
         self.all_cars = []
         self.add_cars(10)
 
@@ -28,3 +27,13 @@ class CarManager(Turtle):
             new_car.goto(random.randint(-50, 800), random.randint(-250, 250))
             self.all_cars.append(new_car)
 
+    # def example_create_car(self):
+    #     random_chance = random.randint(1, 6)
+    #     if random_chance == 1:
+    #         new_car = Turtle("square")
+    #         new_car.shapesize(stretch_len=2, stretch_wid=1)
+    #         new_car.penup()
+    #         new_car.color(random.choice(COLORS))
+    #         random_y = random.randint(-250, 250)
+    #         new_car.goto(300, random_y)
+    #         self.all_cars.append(new_car)
