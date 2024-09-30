c3b88ee88907e0d0afdc3b35ee9d469c0ce118c6
J MacPherson <jtdmacpherson@pinkieboi.com>
2024-09-30 12:46:43 +0100

    cleanup code

--- 033_ISS_Tracker/ISS_Tracker/main.py
+++ 033_ISS_Tracker/ISS_Tracker/main.py
@@ -1,18 +1,36 @@
+import time
 import smtplib
 import requests
 import datetime as dt
 
 
+def is_overhead():
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    iss_position = response.json()["iss_position"]
+    if -0.070340 > float(iss_position["longitude"]) < -0.303134:
+        if abs(params["lat"]) < float(iss_position["latitude"] > abs(params["lat"]) + 1):
+            return True
+
+
+def is_night():
+    nighttime = requests.get(url="https://api.sunrise-sunset.org/json?", params=params)
+    sunset = nighttime.json()["results"]["sunset"].split("T")[1].split(":")[:2]
+    current_time = [(dt.datetime.today().hour - 12), dt.datetime.today().minute]
+    if current_time[0] > int(sunset[0]):
+        return True
+    elif current_time[0] == int(sunset[0]) and current_time[1] >= sunset[1]:
+        return True
+
+
 def send_alert():
-    if float(iss_position["longitude"]) == params["lat"] and float(iss_position["latitude"]) == params["lng"]:
-        with smtplib.SMTP("smtp.gmail.com", port=587) as server:
-            server.starttls()
-            server.login(user=EMAIl, password=PASSCODE)
-            server.sendmail(
-                from_addr=EMAIl,
-                to_addrs="",
-                msg="Subject:ISS Overhead\n\nLook up, the ISS is passing overhead!"
-            )
+    with smtplib.SMTP("smtp.gmail.com", port=587) as server:
+        server.starttls()
+        server.login(user=EMAIl, password=PASSCODE)
+        server.sendmail(
+            from_addr=EMAIl,
+            to_addrs="",
+            msg="Subject:ISS Overhead\n\nLook up, the ISS is passing overhead!"
+        )
 
 
 with open("secrets.txt") as s:
@@ -21,22 +39,10 @@ with open("secrets.txt") as s:
 
 params = {
     "lat": 50.827778,
-    "lng": -0.152778
+    "lng": -0.152778,
+    "formatted": 0
 }
 
-# Get ISS position
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
-iss_position = response.json()["iss_position"]
-
-if dt.datetime.today().hour > 12:
-    # Get nighttime hours
-    nighttime = requests.get(url="https://api.sunrise-sunset.org/json?", params=params)
-    sunset = nighttime.json()["results"]["sunset"].split()[0]
-    sunset = sunset.split(":")
-    current_time = [(dt.datetime.today().hour - 12), dt.datetime.today().minute]
-
-    if current_time[0] < int(sunset[0]):
-        send_alert()
-    elif current_time[0] == int(sunset[0]) and current_time[1] <= sunset[1]:
-        send_alert()
-
+while is_overhead() and is_night():
+    send_alert()
+    time.sleep(60)
