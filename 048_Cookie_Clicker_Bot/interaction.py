14475996c2d2acf2652fa2594840268722cb75f9
J MacPherson <jtdmacpherson@pinkieboi.com>
2024-10-31 23:13:33 +0000

    selenium

--- 048_Cookie_Clicker_Bot/interaction.py
+++ 048_Cookie_Clicker_Bot/interaction.py
@@ -1,12 +1,14 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 chrome_opts = webdriver.ChromeOptions()
 chrome_opts.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=chrome_opts)
 
-driver.get(url="https://en.wikipedia.org/wiki/Main_Page")
+# driver.get(url="https://en.wikipedia.org/wiki/Main_Page")
+# en_article_count = driver.find_element("css selector", "#articlecount > a:nth-child(1)")
+# en_article_count.click()
 
-en_article_count = driver.find_element("css selector", "#articlecount > a:nth-child(1)")
-en_article_count.click()
-
-# driver.quit()
+driver.get("http://secure-retreat-92358.herokuapp.com/")
+input_field = driver.find_element("class name", "form-control")
+input_field.send_keys("James", Keys.TAB, "MacPherson", Keys.TAB, "email@example.com", Keys.RETURN)
--- 048_Cookie_Clicker_Bot/main.py
+++ 048_Cookie_Clicker_Bot/main.py
@@ -1,21 +1,45 @@
+import time
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 
 
-def page_init():
-    driver.find_element("class name", "fc-primary-button").click()
-    driver.implicitly_wait(2)
-    driver.find_element("class name", "cc_btn_accept_all").click()
-    driver.implicitly_wait(2)
-    driver.find_element("id", "langSelect-EN").click()
-    driver.implicitly_wait(2)
+def cookie_consent():
+    driver.find_element(By.CLASS_NAME, "fc-primary-button").click()
+    time.sleep(10)
+    driver.find_element(By.CLASS_NAME, "cc_btn_accept_all").click()
+    time.sleep(10)
+    driver.find_element(By.ID, "langSelect-EN").click()
+    time.sleep(10)
+
+
+def click_cookie():
+    cookie = driver.find_element(By.ID, "bigCookie")
+    cookie.click()
+
+
+def activate_helper():
+    helpers = driver.find_elements(By.CSS_SELECTOR, ".product.unlocked.enabled")
+    if len(helpers) > 0:
+        helpers[-1].click()
 
 
 chrome_opts = webdriver.ChromeOptions()
 chrome_opts.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=chrome_opts)
 driver.get(url="https://orteil.dashnet.org/cookieclicker/")
-page_init()
+cookie_consent()
 
-cookie = driver.find_element("id", "bigCookie")
-ame_aids = driver.find_elements("class name", "product")
+timeout = time.time() + 5
+five_mins = time.time() + 60 * 5
+
+while True:
+    click_cookie()
+
+    if time.time() > timeout:
+        activate_helper()
+        timeout = time.time() + 5
+
+    if time.time() > five_mins:
+        c_per_sec = driver.find_element(By.ID, "cookiesPerSecond").text
+        print(c_per_sec)
+        break
